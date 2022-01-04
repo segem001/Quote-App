3ed4f554e5971c9e7cd0a08512c617c4f2690042
segem001 <kipdom001@gmail.com>
2022-01-05 00:24:52 +0300

    Add other angular components

---
+++ src/app/downvote/downvote.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DownvoteComponent } from './downvote.component';
+
+describe('DownvoteComponent', () => {
+  let component: DownvoteComponent;
+  let fixture: ComponentFixture<DownvoteComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DownvoteComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DownvoteComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
