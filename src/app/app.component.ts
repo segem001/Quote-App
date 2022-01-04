c88b37083e037a3ea4ac19d0e114cb649dd23b52
segem001 <kipdom001@gmail.com>
2022-01-04 22:58:27 +0300

    modify the the exported class appcomponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,13 @@
 import { Component } from '@angular/core';
 import { Quote } from './quote';
 
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Quote-Application';
+
+ 
+}
